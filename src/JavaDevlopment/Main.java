932bcde48a145b63328991f1df44288cb3113624
NagrajSrivatsa15 <nagrajsrivatsa1995@gmail.com>
2021-05-05 16:44:25 +0100

    Reverrse Array

---
+++ src/JavaDevlopment/Main.java
@@ -0,0 +1,25 @@
+package JavaDevlopment;
+
+import java.util.Arrays;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        int [] array={1,2,3,4,5,6};
+        System.out.println("before reversal array" + Arrays.toString(array));
+        Reverse(array);
+        System.out.println("After reversing array" +Arrays.toString(array));
+    }
+
+    public static void Reverse(int[] array){
+
+        int maxIndex=array.length-1;
+        int halflength=array.length/2;
+        for(int i=0; i<halflength;i++){
+            int temp=array[i];
+            array[i]=array[maxIndex-i];
+            array[maxIndex-i]=temp;
+        }
+    }
+}
